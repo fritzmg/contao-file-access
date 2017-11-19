e7e74648113ae49fd7df4635f7c5cdec90422be9
fritzmg <fmg@inspiredminds.at>
2017-11-19 21:15:00 +0100

    implement #2

--- src/system/modules/file_access/dca/tl_files.php
+++ src/system/modules/file_access/dca/tl_files.php
@@ -20,6 +20,7 @@ $GLOBALS['TL_DCA']['tl_files']['fields']['protected'] = array
 	'exclude'                 => true,
 	'filter'                  => true,
 	'inputType'               => 'checkbox',
+	'save_callback'           => array(array('tl_files_file_access', 'protectFolder')),
 	'eval'                    => array('tl_class'=>'clr'),
 	'sql'                     => "char(1) NOT NULL default ''"
 );
@@ -34,3 +35,57 @@ $GLOBALS['TL_DCA']['tl_files']['fields']['groups'] = array
 	'sql'                     => "blob NULL",
 	'relation'                => array('type'=>'hasMany', 'load'=>'lazy')
 );
+
+
+/**
+ * Helper class
+ */
+class tl_files_file_access
+{
+	/**
+	 * Return a checkbox to delete session data
+	 *
+	 * @param DataContainer $dc
+	 *
+	 * @return string
+	 */
+	public function protectFolder($varValue, DataContainer $dc)
+	{
+		$strPath = $dc->id;
+
+		// Check whether the temporary name has been replaced already (see #6432)
+		if (\Input::post('name'))
+		{
+			if (\Validator::isInsecurePath(\Input::post('name')))
+			{
+				throw new \RuntimeException('Invalid file or folder name ' . \Input::post('name'));
+			}
+
+			$count = 0;
+			$strName = basename($strPath);
+
+			if (($strNewPath = str_replace($strName, \Input::post('name'), $strPath, $count)) && $count > 0 && is_dir(TL_ROOT . '/' . $strNewPath))
+			{
+				$strPath = $strNewPath;
+			}
+		}
+
+		// Only show for folders (see #5660)
+		if (!is_dir(TL_ROOT . '/' . $strPath))
+		{
+			return '';
+		}
+
+		// Protect or unprotect the folder
+		if ($varValue)
+		{
+			\File::putContent($strPath . '/.htaccess', '');
+		}
+		else
+		{
+			@unlink($strPath . '/.htaccess');
+		}
+
+		return $varValue;
+	}
+}
