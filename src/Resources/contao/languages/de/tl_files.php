392d659d6937915b75ce27c91ab19ff0aaf18299
Fritz Michael Gschwantner <fmg@inspiredminds.at>
2019-03-28 11:34:21 +0100

    update translations

--- src/Resources/contao/languages/de/tl_files.php
+++ src/Resources/contao/languages/de/tl_files.php
@@ -10,5 +10,4 @@ declare(strict_types=1);
  * @license LGPL-3.0-or-later
  */
 
-$GLOBALS['TL_LANG']['tl_files']['protected'][1] = 'Verhindert den Zugriff auf die Datei oder den Ordner via HTTP wenn die file.php benutzt wird. Die Dateien sind weiterhin z.B. über das Downloads-Element verfügbar.';
-$GLOBALS['TL_LANG']['tl_files']['groups'] = ['Erlaubte Mitgliedergruppen', 'Diese Gruppen können auf die Datei oder den Ordner zugreifen.'];
+$GLOBALS['TL_LANG']['tl_files']['groups'] = ['Erlaubte Mitgliedergruppen', 'Diese Gruppen können auf den Ordner zugreifen.'];
--- src/Resources/contao/languages/en/tl_files.php
+++ src/Resources/contao/languages/en/tl_files.php
@@ -10,5 +10,4 @@ declare(strict_types=1);
  * @license LGPL-3.0-or-later
  */
 
-$GLOBALS['TL_LANG']['tl_files']['protected'][1] = 'Prevent accessing the file or folder via HTTP when the file.php is in use. The files can still be made available e.g. with the downloads element.';
-$GLOBALS['TL_LANG']['tl_files']['groups'] = ['Allowed member groups', 'These groups will be able to access the file or folder.'];
+$GLOBALS['TL_LANG']['tl_files']['groups'] = ['Allowed member groups', 'These groups will be able to access the folder.'];
