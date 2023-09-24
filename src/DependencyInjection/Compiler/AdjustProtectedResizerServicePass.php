28720494df00d8ef0f1bd8e8329cc55b5c830ee7
Fritz Michael Gschwantner <fmg@inspiredminds.at>
2023-09-25 00:00:42 +0100

    some fixes

--- src/DependencyInjection/Compiler/AdjustProtectedResizerServicePass.php
+++ src/DependencyInjection/Compiler/AdjustProtectedResizerServicePass.php
@@ -13,22 +13,23 @@ declare(strict_types=1);
 namespace InspiredMinds\ContaoFileAccessBundle\DependencyInjection\Compiler;
 
 use Contao\CoreBundle\Image\LegacyResizer;
+use Contao\CoreBundle\Image\Resizer;
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 
+/**
+ * Adjusts some service definitions for Contao 5.
+ */
 class AdjustProtectedResizerServicePass implements CompilerPassInterface
 {
     public function process(ContainerBuilder $container): void
     {
-        if (!$container->has('contao_file_access.image.protected_resizer')) {
-            return;
-        }
+        if ($container->has('contao_file_access.image.protected_resizer')) {
+            $definition = $container->getDefinition('contao_file_access.image.protected_resizer');
 
-        $definition = $container->getDefinition('contao_file_access.image.protected_resizer');
-
-        // BC for Contao 4.13
-        if (class_exists(LegacyResizer::class)) {
-            $definition->setClass(LegacyResizer::class);
+            if (!class_exists(LegacyResizer::class)) {
+                $definition->setClass(Resizer::class);
+            }
         }
     }
 }
